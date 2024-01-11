8766e33cec08e701eee2936bb3ae5b7d08f49696
Dap Moderator <123246207+thebanon@users.noreply.github.com>
2024-01-11 18:22:17 -0500

    Update Frontend
    Create Code Page
    Update Index Files
    Import QRCode JS Libray

---
+++ code/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var typeNumber = 4;
+    var errorCorrectionLevel = 'L';
+    var qr = qrcode(typeNumber, errorCorrectionLevel);
+    qr.addData('Hi!');
+    qr.make();
+    document.getElementById('qrcode').innerHTML = qr.createImgTag();
+}
